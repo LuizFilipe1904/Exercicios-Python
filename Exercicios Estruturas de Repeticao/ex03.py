e9fe0a17cb78e1e9adbf1be4f0d40c575421838f
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-12 17:03:51 -0300

    Update ex03.py

--- Exercicios Estruturas de Repeticao/ex03.py
+++ Exercicios Estruturas de Repeticao/ex03.py
@@ -1,3 +1,14 @@
+'''
+Faça um programa que leia e valide as seguintes informações:
+    Nome: maior que 3 caracteres;
+    Idade: entre 0 e 150;
+    Salário: maior que zero;
+    Sexo: 'f' ou 'm';
+    Estado Civil: 's', 'c', 'v', 'd';
+    Use a função len(string) para saber o tamanho de um texto (número de caracteres).
+'''
+
+
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 sal = float(input("Digite seu salário: "))
