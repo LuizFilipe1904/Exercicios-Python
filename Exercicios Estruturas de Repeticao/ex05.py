349cc871ff8baacb19650f12ede7f688c33eca84
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-12 17:05:22 -0300

    Update ex05.py

--- Exercicios Estruturas de Repeticao/ex05.py
+++ Exercicios Estruturas de Repeticao/ex05.py
@@ -1,3 +1,8 @@
+'''
+Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. 
+Valide a entrada e permita repetir a operação.
+'''
+
 ano = 0
 a = int(input("Digite a população do país A: "))
 b = int(input("Digite a população do país B: "))
