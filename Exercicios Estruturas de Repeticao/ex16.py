cd4a1fa6dfe6dd7eb45ac52b4fdcaa9ed63a1160
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-12 16:59:44 -0300

    Add files via upload

---
+++ Exercicios Estruturas de Repeticao/ex16.py
@@ -0,0 +1,13 @@
+'''
+A série de Fibonacci é formada pela sequência 0,1,1,2,3,5,8,13,21,34,55,...
+Faça um programa que gere a série até que o valor seja maior que 500.
+'''
+
+primeiro = 0
+segundo = 1
+
+for i in range(501):
+    termo = primeiro + segundo
+    segundo = primeiro
+    primeiro = termo
+    print(termo)
