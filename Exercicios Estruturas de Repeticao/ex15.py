9314a740f9972df1f503be4fef5f98957e75a8c1
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-12 17:08:41 -0300

    Update ex15.py

--- Exercicios Estruturas de Repeticao/ex15.py
+++ Exercicios Estruturas de Repeticao/ex15.py
@@ -1,7 +1,6 @@
 '''
 A série de Fibonacci é formada pela sequência 1,1,2,3,5,8,13,21,34,55,... 
 Faça um programa capaz de gerar a série até o n-ésimo termo.
-
 '''
 
 n = int(input("Informe qual número da sequência deseja saber: "))
