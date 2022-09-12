8d7e55020b9c8a8f874c4bf9eb1824faf77bb2a6
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-12 17:07:58 -0300

    Update ex13.py

--- Exercicios Estruturas de Repeticao/ex13.py
+++ Exercicios Estruturas de Repeticao/ex13.py
@@ -1,3 +1,8 @@
+'''
+Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. 
+Não utilize a função de potência da linguagem.
+'''
+
 base = int(input("Digite o número base: "))
 exp = int(input("Digirte o expoente: "))
 pot = 1
@@ -6,5 +11,3 @@ for i in range(exp):
     pot *= base
 
 print(f"O resultado foi de {pot}")
-
-
