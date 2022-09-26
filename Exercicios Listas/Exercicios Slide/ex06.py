9076b8254c6fb7476d83e5d9c47b63c2329398eb
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-26 17:57:43 -0300

    Update ex06.py

--- Exercicios Listas/Exercicios Slide/ex06.py
+++ Exercicios Listas/Exercicios Slide/ex06.py
@@ -2,19 +2,14 @@
 Escreva um programa que crie uma lista com todos a sequência de Fibonacci até o termo que seu usuário desejar.
 '''
 
-n = int(input('Informe até qual número deseja gerar a sequência de Fibonacci: '))
-seq = []
-ult = 1
-pen = 1
+n = int(input('Quantos números deseja mostrar: '))
+t1 = 1
+t2 = 1
+seq = [1, 1]
 
-if n == 1 or n == 2:
-    print('1')
-else:
-    for i in range(2, n):
-        termo = ult + pen
-        pen = ult
-        ult = termo
-        i += 1
-        seq.append(termo)
-
-print(termo)
+for cont in range(2, n):
+    t3 = t1 + t2
+    t2 = t1
+    t1 = t3
+    seq.append(t3)
+print(seq)
