f7148b58d0d3ad43798d326cec68c63fd09101eb
Luiz Filipe Carvalho <65690834+LuizFilipe1904@users.noreply.github.com>
2022-09-26 12:31:57 -0300

    Delete ex09.py

--- Exercicios Listas/Exercicios Slide/ex09.py
+++
@@ -1,6 +0,0 @@
-'''
-Crie uma lista palavras com dez palavras e em seguida.
-a. crie uma nova lista ord com o conteúdo de palavras em ordem alfabética
-b. crie uma lista inv com o conteúdo de ord em ordem inversa.
-c. imprima palavras, ord e inv
-'''
